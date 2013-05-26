5ee99d3367144a9c3077d90f30e2a5688e6d5f59
Mark Maglana <mmaglana@gmail.com>
2013-05-25 21:25:42 -0700

    Test the controller directly instead of using Session#projects

--- test/lib/redstack/controllers/identity/projects_controller_test.rb
+++ test/lib/redstack/controllers/identity/projects_controller_test.rb
@@ -1,14 +1,15 @@
 require_relative '../../../../test_helper'
 
-include RedStack::Controllers
-
 describe 'RedStack::Controllers::ProjectsController' do
 
-  it 'retrieves projects of the currently logged in user' do
+  before do
     @os = RedStack::Session.new(host: 'http://devstack:5000', api_version: 'v2.0', stub_openstack: true)
     @os.authenticate username: 'validuser', password: '123qwe'
-    
-    projects = @os.projects.find
+    @controller = RedStack::Controllers::Identity::ProjectsController.new(@os)
+  end
+
+  it 'retrieves projects of the currently logged in user' do
+    projects = @controller.find
     
     projects.must_be_instance_of Array
     projects.length.wont_be_nil
