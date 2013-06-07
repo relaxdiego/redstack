dd37a78bce7092b17710174626e4d6549f89df04
Mark Maglana <mmaglana@gmail.com>
2013-06-07 15:38:45 -0700

    Refactor Token::do_create

--- lib/redstack/identity/models/token.rb
+++ lib/redstack/identity/models/token.rb
@@ -1,21 +1,21 @@
 module RedStack
 module Identity
 module Models
-    
+
   # http://docs.openstack.org/api/openstack-identity-service/2.0/content/Token_Operations.html
   class Token < RedStack::Base::Model
-    
+
     attribute :id
     attribute :issued_at
     attribute :expires
-    attribute :project, key: 'tenant' 
-    
+    attribute :project, key: 'tenant'
+
     def initialize(options = {})
       @services      = options[:data]['access']['serviceCatalog']
       options[:data] = options[:data]['access']
       super options
     end
-                
+
     def get_endpoint(options = {})
       return nil if (is_default? || error)
       raise(ArgumentError.new('service not supplied')) if options[:service].nil?
@@ -26,22 +26,22 @@ module Models
         'public'   => 'publicURL',
         'internal' => 'internalURL'
       }
-      
+
       @services.find { |s| s['type'] == options[:service] }['endpoints'][0][types[options[:type]]]
     end
-    
-    
+
+
     def is_default?
       self[:project].nil? unless error
     end
     alias :is_unscoped? :is_default?
-    
-    
+
+
     def is_scoped?
       !is_default?
     end
-    
-    
+
+
     def validate(options={})
       if self.is_default?
         raise RedStack::NotAuthorizedError.new("token with id #{ self[:id] } is not authorized to validate other tokens")
@@ -69,13 +69,13 @@ module Models
         return false, JSON.parse(response.body)['error']
       end
     end
-    
-    
+
+
     private
-    
+
     def self.build_attributes(attributes)
       body = {}
-      
+
       body[:auth] = if attributes.has_key? :username
                       {
                         passwordCredentials: {
@@ -96,24 +96,24 @@ module Models
     end
 
     def self.do_create(options)
-      attributes = options[:attributes] || raise(ArgumentError.new('attributes not supplied'))
-      connection = options[:connection] || raise(ArgumentError.new('connection not supplied'))
-      stub_path  = connection.url_prefix.path + '/' + resource_path
+      attributes     = options[:attributes] || raise(ArgumentError.new('attributes not supplied'))
+      connection     = options[:connection] || raise(ArgumentError.new('connection not supplied'))
+      mock_data_path = connection.url_prefix.path + '/' + resource_path
 
       response = nil
-      VCR.use_cassette(stub_path, record: :new_episodes, match_requests_on: [:uri, :headers, :body, :method]) do
+      VCR.use_cassette(mock_data_path, record: :new_episodes, match_requests_on: [:uri, :headers, :body, :method]) do
         response = connection.post do |req|
           req.url resource_path
           req.body = build_attributes(attributes).to_json
         end
       end
-      
+
       response
     end
-                
+
   end # class Token
 
 end # module Models
 end # module Identity
 end # module RedStack
-  
+
