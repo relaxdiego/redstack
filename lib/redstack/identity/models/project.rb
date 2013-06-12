a47d481c753152011a1e5b1991ecef6b1065f919
Mark Maglana <mmaglana@gmail.com>
2013-06-12 12:16:45 -0700

    Remove trailing whitespaces

--- lib/redstack/identity/models/project.rb
+++ lib/redstack/identity/models/project.rb
@@ -1,13 +1,13 @@
 module RedStack
 module Identity
 module Models
-  
-  # http://docs.openstack.org/api/openstack-identity-service/2.0/content/Tenant_Operations.html 
+
+  # http://docs.openstack.org/api/openstack-identity-service/2.0/content/Tenant_Operations.html
   class Project < RedStack::Base::Model
 
     resource  'tenant'
 
-    attribute :id      
+    attribute :id
     attribute :name
     attribute :description
     attribute :enabled,     default: true
@@ -26,4 +26,4 @@ module Models
 end # module Models
 end # module Identity
 end # module RedStack
-  
+
