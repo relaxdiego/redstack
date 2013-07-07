5e99f0bb6ee80b0d7f88072336f83b7c9a62c0c1
Mark Maglana <mmaglana@gmail.com>
2013-07-06 23:12:47 -0700

    Minor fixes to Connection tests

--- test/redstack/identity/connection_test.rb
+++ test/redstack/identity/connection_test.rb
@@ -5,16 +5,16 @@ class RedStack::Identity::ConnectionTest < MiniTest::Spec
 
   describe 'RedStack::Identity::Connection' do
 
-    describe '#new' do
+    describe '::new' do
 
-      it 'returns a Connection instance' do
+      it 'creates a Connection instance' do
         ks = Connection.new host: 'http://os.com', api_version: 'v2.0'
 
-        ks.class.must_equal RedStack::Identity::Connection
+        ks.class.must_equal Connection
       end
 
 
-      it 'raises an error when the host parameter is missing' do
+      it 'raises an error when host is missing' do
         initializer = lambda { Connection.new api_version: 'v2.0' }
         initializer.must_raise ArgumentError
 
@@ -24,7 +24,7 @@ class RedStack::Identity::ConnectionTest < MiniTest::Spec
       end
 
 
-      it 'raises an error when the api_version parameter is missing' do
+      it 'raises an error when api_version is missing' do
         initializer = lambda { Connection.new host: 'http://www.example.org' }
         initializer.must_raise ArgumentError
 
@@ -43,7 +43,7 @@ class RedStack::Identity::ConnectionTest < MiniTest::Spec
         error.message.wont_be_nil
       end
 
-    end # describe '#new'
+    end # describe '::new'
 
 
     describe '#create_token' do
@@ -51,13 +51,6 @@ class RedStack::Identity::ConnectionTest < MiniTest::Spec
       def conn
         Connection.new host: 'http://devstack:5000', api_version: 'v2.0'
       end
-
-      it 'returns a token' do
-        token = conn.create_token username: 'validuser', password: 'validpassword'
-
-        token.class.must_equal Token
-      end
-
 
       it 'raises an error when username, password, and token are missing' do
         m = lambda { conn.create_token }
@@ -69,20 +62,20 @@ class RedStack::Identity::ConnectionTest < MiniTest::Spec
       end
 
 
-      it 'does not require token when username and password are present' do
+      it 'creates a token when username and password are provided' do
         token = conn.create_token username: 'validuser', password: 'validpassword'
 
         token.class.must_equal Token
       end
-      
-      
-      it 'does not require username and password when a token is present' do
+
+
+      it 'creates a token when another token is provided' do
         token = conn.create_token token: 'sometokenvaluehere'
 
         token.class.must_equal Token
       end
 
-    end
+    end # describe '#create_token'
 
   end
 
