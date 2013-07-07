093e4cb189b195aef9deadc4f006ce06c2acfc76
Mark Maglana <mmaglana@gmail.com>
2013-07-07 15:23:19 -0700

    Minor code cleanup

--- test/support/test_reporter.rb
+++ test/support/test_reporter.rb
@@ -2,28 +2,26 @@
 unless ENV['CI'] || ENV['TRAVIS']
   require "minitest/reporters"
 
-  module MiniTest
-  module Reporters
-  module RedStack
-        
+  module MiniTest::Reporters::RedStack
+
     class SpecReporter < MiniTest::Reporters::SpecReporter
 
       private
-    
+
       def pad_test(test)
         str = test.to_s.gsub(/(test_)/, '').gsub(/_/, ' ')
         pad("%-#{TEST_SIZE}s" % str, TEST_PADDING)[0..TEST_SIZE]
       end
-    
+
       def print_info(e)
         print "       #{e.exception.class.to_s}:\n"
         e.message.each_line { |line| print_with_info_padding(line) }
-    
+
         trace = filter_backtrace(e.backtrace)
-        
+
         # TODO: Use the proper MiniTest way of customizing the filter
         trace.each { |line| print_with_info_padding(line) unless line =~ /\.rvm|gems|_run_anything/ }
-      end    
+      end
 
       def print_suite(suite)
         puts suite.name.gsub('::#', '#')
@@ -31,10 +29,11 @@ unless ENV['CI'] || ENV['TRAVIS']
       end
     end
 
+
     class ProgressReporter < MiniTest::Reporters::ProgressReporter
-      
+
       private
-      
+
       def print_test_with_time(suite, test)
         total_time = Time.now - (runner.test_start_time || Time.now)
         suite_name = suite.name.gsub('::#', '#').gsub('::::', '::')
@@ -43,8 +42,6 @@ unless ENV['CI'] || ENV['TRAVIS']
       end
     end
 
-  end
-  end
   end
 
   MiniTest::Reporters.use! MiniTest::Reporters::RedStack::ProgressReporter.new
